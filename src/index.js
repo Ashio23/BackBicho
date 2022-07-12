d46dbb96eae1e1d21e7be899e12a5bf896b2b7e2
Jonathan González <jgonzalezinfor@gmail.com>
2022-07-11 22:20:12 -0400

    final

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import app from "./app";
 
 const main = () => {
-    app.listen(process.env.PORT || 5000);
+    app.listen(app.get("port"));
     console.log(`Server on port ${app.get("port")}`);
 };
 
